bdc0059939dd3f0bb75ba77d7fe176a120d68668
FLXYDEV <155314684+FLXYDEV@users.noreply.github.com>
2025-04-05 17:15:48 +0200

    added typescript type shi

--- canvasGame.js
+++ canvasGame.js
@@ -1,14 +1,30 @@
+
 let mapSize = 32;
 let mapTiles = [];
 
+/** @type {HTMLCanvasElement} */ 
 let mainCanvas = document.getElementById("mainCanvas");
+/** @type {CanvasRenderingContext2D} */
 let ctx = mainCanvas.getContext("2d");
 
-for(let x = 0; x < mapSize; x++) {
-    mapTiles[x] = [];
-    for(let y = 0; y < mapSize; y++) {
-        mapTiles[x][y] = x+y;
-    }
+let imgData = ctx.createImageData(64,64);
+
+for(let i = 0; i < imgData.data.length; i += 4) {
+    imgData.data[i+0] = 255;
+    imgData.data[i+1] = 255;
+    imgData.data[i+2] = 127;
+    imgData.data[i+3] = 255;
 }
+ctx.putImageData(imgData, 64, 64); 
+
+ctx.putImageData
 
 
+//for(let x = 0; x < mapSize; x++) {
+//    mapTiles[x] = [];
+//    for(let y = 0; y < mapSize; y++) {
+//        mapTiles[x][y] = x+y;
+//        ctx.fillStyle = (x+y)%2==1 ? "gray": "black";
+//        var col = ctx.fillRect(x*8, y*8, 8, 8);
+//    }
+//}
--- game.js
+++ game.js
@@ -1,20 +1,7 @@
-let mapSize = 16;
-let mapTiles
 
 let button = document.getElementById("startButton");
 let text = document.getElementById("testingText");
 
 button.onclick = () => {
-    //if(mapTiles == null) {
-        mapTiles = [];
-        for(let x = 0; x < mapSize; x++) {
-            mapTiles[x] = [];
-            for(let y = 0; y < mapSize; y++) {
-                mapTiles[x][y] = x+y;
-            }
-        }
-    //}
-    let randomX = Math.ceil(Math.random()*mapSize-1);
-    let randomY = Math.ceil(Math.random()*mapSize-1);
-    text.innerHTML = mapTiles[randomX][randomY];
+    text.innerHTML = Math.floor(Math.random()*101);
 }
